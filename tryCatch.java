bc3f98e638bba05797727418f6fc416ff87f63ef
Harsh Singh <harshsinghcgu@gmail.com>
2025-03-28 09:46:11 +0530

    Add files via upload

---
+++ tryCatch.java
@@ -0,0 +1,11 @@
+public class tryCatch {
+    public static void main(String[] args) {
+        try{
+            int data=50/0;
+        }
+        catch(ArithmeticException e){
+            System.out.println(e);
+
+        }System.out.println("Rest of code");
+    }
+}
